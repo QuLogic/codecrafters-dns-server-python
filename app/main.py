ec0fe7538095cbbca730773873e6369444a907c6
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2023-11-29 00:58:59 -0500

    Add a dataclass for a DNS header

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import dataclasses
 import socket
 
 
@@ -15,6 +16,41 @@ def main():
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
+
+
+def bit_field(width):
+    """Create a bit field with given *width*."""
+    return dataclasses.field(metadata={'width': width})
+
+
+@dataclasses.dataclass(kw_only=True)
+class DNSHeader:
+    """A DNS header."""
+
+    packet_identifier: int = bit_field(16)
+    query_response: int = bit_field(1)
+    operation_code: int = bit_field(4)
+    authoritative_answer: int = bit_field(1)
+    truncation: int = bit_field(1)
+    recursion_desired: int = bit_field(1)
+    recursion_available: int = bit_field(1)
+    reserved: int = bit_field(3)
+    response_code: int = bit_field(4)
+    question_count: int = bit_field(16)
+    answer_record_count: int = bit_field(16)
+    authority_record_count: int = bit_field(16)
+    additional_record_count: int = bit_field(16)
+
+    def __post_init__(self) -> None:
+        """Validate fields fit within specified width."""
+        for field in dataclasses.fields(self):
+            if 'width' not in field.metadata:
+                raise TypeError(f'{field.name} is not annotated with bit field width')
+            max_value = 2 ** field.metadata['width'] - 1
+            value = getattr(self, field.name)
+            if value < 0 or value > max_value:
+                raise ValueError(
+                    f'{field.name} ({value}) is out of bounds [0, {max_value}]')
 
 
 if __name__ == "__main__":
